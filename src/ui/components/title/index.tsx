12d86c7d2f1847daa54f10e5eb34ade9580c8963
João Alberto <hey@joaoalberto.dev>
2022-01-09 23:48:16 -0300

    Change title size

--- src/ui/components/title/index.tsx
+++ src/ui/components/title/index.tsx
@@ -3,7 +3,7 @@ import { styled } from "@/ui/config/theme";
 const Title = styled("h1", {
   color: '$green11',
   fontFamily: "$heading",
-  fontSize: "$subheading",
+  fontSize: "$heading",
   fontWeight: "bold",
   margin: "0",
 });
