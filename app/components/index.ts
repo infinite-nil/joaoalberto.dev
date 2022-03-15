0839a01c9ca8e6723b7e06f62ab8417038568ccc
João Alberto <hey@joaoalberto.dev>
2022-03-15 17:15:24 -0300

    chore: create components

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,1 +1,3 @@
 export * from "./layout";
+export * from "./paragraph";
+export * from "./title";
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,28 +1,30 @@
+import { Paragraph, Title } from "~/components";
+
 export default function Index() {
   return (
     <main className="md:grid sm:grid-cols-2 md:grid-cols-6">
       <header className="col-span-5">
-        <h1 className="text-3xl md:text-5xl md:leading-snug mt-9 md:mt-24 font-bold mb-24">
+        <Title>
           Hello there, I'm <span className="gradient-text">João Alberto</span>,
           software developer helping to build a better web.
-        </h1>
+        </Title>
       </header>
       <div className="col-span-4 col-start-1">
-        <p className="md:text-xl leading-relaxed text-gray-500">
+        <Paragraph>
           I am a brazilian self-taught developer with seven years of experience
           developing for web.
-        </p>
+        </Paragraph>
         <p className="text-xl md:text-3xl font-bold mb-3 mt-9">What I do</p>
-        <p className="md:text-xl leading-relaxed text-gray-500">
+        <Paragraph>
           Mostly working on the front-end using React but also in the back-end
           using Node.js and Ruby.
-        </p>
+        </Paragraph>
         <p className="text-xl md:text-3xl font-bold mb-3 mt-9">What I like.</p>
-        <p className="md:text-xl leading-relaxed text-gray-500">
+        <Paragraph>
           My interests are programming languages, types systems and algorithms
           and web3. When not working for my employeer or some side project I'm
           probably reading about one of these topics.
-        </p>
+        </Paragraph>
       </div>
     </main>
   );
