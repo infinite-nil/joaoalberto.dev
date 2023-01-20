569ebf919e0df9f8824f9b686481a8ef0de04fbd
João Alberto <hey@joaoalberto.dev>
2023-01-20 15:46:14 -0300

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://joaoalberto.dev",
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "server",
-  adapter: vercel(),
 });
