4eff9ec6209943f21a43328013b9e6a4820a705e
João Alberto <hey@joaoalberto.dev>
2023-01-19 23:32:57 -0300

    fix site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://joaoalberto.dev",
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://new.joaoalberto.dev",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
