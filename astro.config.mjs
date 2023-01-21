fa8f689d2d333cf47a94e1185c7e70a0decd88ef
João Alberto <hey@joaoalberto.dev>
2023-01-21 02:44:16 -0300

    adding analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://joaoalberto.dev",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), partytown()],
 });
