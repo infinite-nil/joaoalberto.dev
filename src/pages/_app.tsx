6a564b7e13d1a552deaf6a501de32f6333c43bca
João Alberto <hey@joaoalberto.dev>
2023-03-24 18:11:57 -0300

    new

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+
+const satoshi = localFont({ src: "./fonts/Satoshi-Variable.ttf" });
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className={`app-container ${satoshi.className}`}>
+      <Component {...pageProps} />;
+    </div>
+  );
+}
