dd9c64ce2363a284a9041affa4fd8c5a6e60630b
João Alberto <hey@joaoalberto.dev>
2023-08-10 04:43:36 -0300

    clear

--- src/utils/getExperiments.ts
+++
@@ -1,7 +0,0 @@
-import { allExperiments } from "contentlayer/generated";
-
-const getExperiments = (limit: number = Infinity) => {
-  return allExperiments.filter((_, i) => i <= limit);
-};
-
-export { getExperiments };
--- src/utils/getResourceSlug.ts
+++
@@ -1,6 +0,0 @@
-const getResourceSlug = (resourcePath: string) => {
-  const resourcePathParts = resourcePath.split("/");
-  return resourcePathParts[resourcePathParts.length - 1];
-};
-
-export { getResourceSlug };
