c316a3574956ef1194569a62e12e4ba56473add7
João Alberto <hey@joaoalberto.dev>
2023-08-11 13:17:50 -0300

    refactor: navbar component

--- src/ui/components/navbar/index.tsx
+++ src/ui/components/navbar/index.tsx
@@ -1,19 +1,10 @@
-import { Text } from "../text";
-
 function Navbar() {
   return (
     <nav className="fixed z-50 w-full overflow-hidden bg-white/90 py-4 text-neutral-600">
-      <div className="mx-auto flex items-center justify-between px-6 md:max-w-3xl">
+      <div className="mx-auto flex items-center justify-center px-6 md:max-w-3xl md:justify-between">
         <h1 className="font-bold">
-          <Text className="m-0 inline-block" big>
-            👨🏽‍💻
-          </Text>
           <a href="/">João Alberto</a>
         </h1>
-        {/* <div className="flex items-center gap-4 text-sm">
-          <a href="/blog">Blog</a>
-          <a href="/lab">Lab</a>
-        </div> */}
       </div>
     </nav>
   );
