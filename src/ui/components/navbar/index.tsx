65be7a2caec3dfd330abd492351d99e18eabe0e9
João Alberto <hey@joaoalberto.dev>
2023-08-11 14:30:49 -0300

    fix: navbar link component

--- src/ui/components/navbar/index.tsx
+++ src/ui/components/navbar/index.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 function Navbar() {
   return (
     <nav className="fixed z-50 w-full overflow-hidden bg-white/90 py-4 text-neutral-600">
       <div className="mx-auto flex items-center justify-center px-6 md:max-w-3xl md:justify-between">
         <h1 className="font-bold">
-          <a href="/">João Alberto</a>
+          <Link href="/">João Alberto</Link>
         </h1>
       </div>
     </nav>
