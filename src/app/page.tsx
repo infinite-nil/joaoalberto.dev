49ab803cd1831120736e235bbe5be58fc042ae35
João Alberto <hey@joaoalberto.dev>
2023-08-18 19:42:36 -0300

    fix: format code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,51 +7,51 @@ import { Text } from "@/ui/components/text";
 import { WorkCard } from "@/ui/components/work-card";
 
 export default function Home() {
-	const projects = getProfileData('projects');
-	const work = getProfileData('experience');
+  const projects = getProfileData("projects");
+  const work = getProfileData("experience");
 
-	return (
-		<>
-			<Container containerStyles="bg-[var(--container-bg)] py-16">
-				<Text big>
-					I am a self-taught software engineer with over{" "}
-					<Link external decorated href="https://www.linkedin.com/in/joao-amo/">
-						8 years of experience
-					</Link>
-					.
-				</Text>
-				<Text big>
-					Currently I&apos;m working at{" "}
-					<Link external decorated href="https://fluency.io/">
-						Fluency Academy
-					</Link>{" "}
-					building real-time experiences that help people to connect with
-					language teachers.
-				</Text>
-				<Text big>
-					Besides my full-time employment I also spent some time writing{" "}
-					<Link external decorated href="https://github.com/infinite-nil">
-						open-source
-					</Link>{" "}
-					software.
-				</Text>
-			</Container>
-			<Container>
-				<Subtitle>Projects</Subtitle>
-				<div className="grid grid-rows-2 gap-8 md:grid-cols-2">
-					{projects.map((project) => (
-						<ProjectCard key={project.url} {...project} />
-					))}
-				</div>
-			</Container>
-			<Container>
-				<Subtitle>Work</Subtitle>
-				<ul className="flex flex-col gap-8">
-					{work.map((work) => (
-						<WorkCard key={work.company} {...work} />
-					))}
-				</ul>
-			</Container>
-		</>
-	);
+  return (
+    <>
+      <Container containerStyles="bg-[var(--container-bg)] py-16">
+        <Text big>
+          I am a self-taught software engineer with over{" "}
+          <Link external decorated href="https://www.linkedin.com/in/joao-amo/">
+            8 years of experience
+          </Link>
+          .
+        </Text>
+        <Text big>
+          Currently I&apos;m working at{" "}
+          <Link external decorated href="https://fluency.io/">
+            Fluency Academy
+          </Link>{" "}
+          building real-time experiences that help people to connect with
+          language teachers.
+        </Text>
+        <Text big>
+          Besides my full-time employment I also spent some time writing{" "}
+          <Link external decorated href="https://github.com/infinite-nil">
+            open-source
+          </Link>{" "}
+          software.
+        </Text>
+      </Container>
+      <Container>
+        <Subtitle>Projects</Subtitle>
+        <div className="grid grid-rows-2 gap-8 md:grid-cols-2">
+          {projects.map((project) => (
+            <ProjectCard key={project.url} {...project} />
+          ))}
+        </div>
+      </Container>
+      <Container>
+        <Subtitle>Work</Subtitle>
+        <ul className="flex flex-col gap-8">
+          {work.map((work) => (
+            <WorkCard key={work.company} {...work} />
+          ))}
+        </ul>
+      </Container>
+    </>
+  );
 }
