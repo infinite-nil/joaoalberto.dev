1aa508d2d0be36194abe59c6a4bd9dc11cfe9acf
João Alberto <hey@joaoalberto.dev>
2023-08-18 20:12:03 -0300

    feat: blog start with contentlayer

---
+++ contentlayer.config.ts
@@ -0,0 +1,22 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+
+const Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: "blog/**/*.md",
+  fields: {
+    title: { type: "string", required: true },
+    date: { type: "date", required: true },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (post) => `/blog/${post._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export { Post };
+export default makeSource({
+  contentDirPath: "src/content",
+  documentTypes: [Post],
+});
