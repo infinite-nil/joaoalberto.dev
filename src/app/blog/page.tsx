4500395a2c25453d0e2cd200e8259a0c33b8cdcd
João Alberto <hey@joaoalberto.dev>
2023-08-19 02:49:34 -0300

    refactor: date format util

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,9 @@
 import { allPosts } from ".contentlayer/generated";
+import { format } from "@/lib/date";
 import { Container } from "@/ui/components/container";
 import { Subtitle } from "@/ui/components/subtitle";
 import { Text } from "@/ui/components/text";
 import Link from "next/link";
-import { format, parseISO } from "date-fns";
 
 function Blog() {
   return (
@@ -15,14 +15,12 @@ function Blog() {
       </Container>
       <Container contentStyles="px-0">
         {allPosts.map((post) => {
-          const date = format(parseISO(post.date), "LLLL d, yyyy");
-
           return (
             <Link key={post._id} href={post.url} className="relative block">
               <div className="p-4">
                 <Text className="mb-2 font-bold">{post.title}</Text>
                 <p className="flex items-center text-xs font-semibold text-neutral-400">
-                  <span>{date}</span>
+                  <span>{format(post.date)}</span>
                 </p>
               </div>
             </Link>
