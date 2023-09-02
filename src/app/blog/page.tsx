4e1d059d8e45c0ecaa024c53bcbd6b74fadfdf9d
João Alberto <hey@joaoalberto.dev>
2023-09-02 19:25:23 -0300

    fix: sort posts by date

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,12 @@
+import { compareDesc } from "date-fns";
 import { allPosts } from ".contentlayer/generated";
 import { BlogCard } from "@/ui/components/blog-card";
 import { Container } from "@/ui/components/container";
 import { Subtitle } from "@/ui/components/subtitle";
 
 function Blog() {
+  const posts = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
+
   return (
     <>
       <Container containerStyles="bg-[var(--container-bg)] py-8">
@@ -12,7 +15,7 @@ function Blog() {
         </Subtitle>
       </Container>
       <Container contentStyles="px-0">
-        {allPosts.map((post) => {
+        {posts.map((post) => {
           return <BlogCard key={post._id} post={post} />;
         })}
       </Container>
