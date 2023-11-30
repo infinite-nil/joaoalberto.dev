71f2cef4e0d690b5a2f637bbf788ecb5d196259e
João Alberto <hey@joaoalberto.dev>
2023-11-30 02:35:33 -0300

    chore: clean

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>João Alberto - software engineer</h1>
-    </main>
-  );
-}
--- src/components/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-type Props = React.PropsWithChildren;
-
-export function BaseLayout({ children }: Props) {
-  return <div className="max-w-[68ch] mx-auto">{children}</div>;
-}
