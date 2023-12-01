c61f88e5fccececf3213cf3998fc719d64ff3579
João Alberto <hey@joaoalberto.dev>
2023-12-01 02:14:49 -0300

    chore: basic post generation

--- build.mjs
+++ build.mjs
@@ -2,16 +2,47 @@
 
 import "zx/globals";
 
-const blog_dir = "./blog/";
-const dir = fs.readdirSync(blog_dir);
+const blog_content = fs.readdirSync(path.join("./blog/"));
+const files = blog_content.map((file) => ({
+  name: file,
+  content: fs.readFileSync(path.join("./", "blog", file)).toString("utf-8"),
+}));
 
-dir.forEach((file) => {
-  try {
-    const post = fs
-      .readFileSync(path.join("./", "blog", file))
-      .toString("utf-8");
-    const lines = post.split("\n");
+for (let file of files) {
+  let blog_layout = fs
+    .readFileSync(path.join("./", "layout", "blog.html"))
+    .toString("utf-8");
 
-    console.log("POST", lines);
-  } catch {}
-});
+  let new_content = [];
+  const lines = file.content.split("\n");
+
+  for (let line of lines) {
+    const pragma = line.match(/<!--(.*?)-->/gi);
+
+    if (pragma && pragma.length > 0) {
+      const p = pragma[0];
+      const t = p.split(":");
+      const n = t[0].replace("<!-- ", "");
+      const v = t[1].replace("-->", "");
+
+      let regex;
+
+      switch (n) {
+        case "title":
+          regex = /<!--title-->/gi;
+          break;
+        case "description":
+          regex = /<!--description-->/gi;
+          break;
+      }
+
+      blog_layout = blog_layout.replace(regex, v.trim());
+    } else {
+      new_content.push(line.trim());
+    }
+  }
+
+  const new_file = blog_layout.replace("<!--content-->", new_content.join(""));
+  fs.ensureDirSync(path.join("./", "build"));
+  fs.writeFileSync(path.join("./", "build", file.name), new_file);
+}
