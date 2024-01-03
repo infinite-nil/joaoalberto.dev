08a8fdaf904a349102d857bed45e1094ba5cba50
João Alberto <hey@joaoalberto.dev>
2024-01-03 19:49:54 -0300

    feat: adding analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import { Syne } from "next/font/google";
 
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`max-w-[68ch] mx-auto ${syne.className}`}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
