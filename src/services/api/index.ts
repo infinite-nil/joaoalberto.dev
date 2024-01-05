72c42018421c034ce0c1ecdbe71cba2281abf42c
João Alberto <hey@joaoalberto.dev>
2024-01-05 14:48:22 -0300

    feat: adding api service

---
+++ src/services/api/index.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { Repository } from "./types";
+
+const client = axios.create({
+  baseURL: "https://api.joaoalberto.dev",
+});
+
+export async function fetchRepositories(): Promise<Repository[]> {
+  try {
+    const { data, status } = await client.get<Repository[]>("/repositories");
+
+    if (status !== 200) return [];
+
+    return data;
+  } catch {
+    return [];
+  }
+}
---
+++ src/services/api/types.ts
@@ -0,0 +1,7 @@
+export type Repository = {
+    description: string;
+    html_url: string;
+    language: string;
+    name: string;
+    topics: string[];
+}
