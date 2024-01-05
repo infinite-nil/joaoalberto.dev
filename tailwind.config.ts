3ab3692334679347690e6beddd11854da7dfed05
João Alberto <hey@joaoalberto.dev>
2024-01-05 18:03:43 -0300

    fix: adding missing varaibles for title and text

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        text: ['var(--font-inter)'],
+        title: ['var(--font-syne)'],
+      },
     },
   },
   plugins: [],
