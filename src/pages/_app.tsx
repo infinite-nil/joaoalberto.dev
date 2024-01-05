df061f362cd256b5608488af8af1521c7402e64f
João Alberto <hey@joaoalberto.dev>
2024-01-05 18:00:36 -0300

    feat: adding inter as default font for body text and change Syne for titles only

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
-import { Syne } from "next/font/google";
+import { Inter, Syne } from "next/font/google";
 
-const syne = Syne({ subsets: ["latin"] });
+const syne = Syne({ subsets: ["latin"], variable: "--font-syne" });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`max-w-[68ch] mx-auto ${syne.className}`}>
+    <main className={`max-w-[68ch] mx-auto ${syne.variable} ${inter.variable} font-text`}>
       <Component {...pageProps} />
       <Analytics />
     </main>
