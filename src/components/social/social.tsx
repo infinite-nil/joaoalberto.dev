a16f45761ce9cb5952f04da600787540c892380b
João Alberto <hey@joaoalberto.dev>
2024-01-05 18:02:17 -0300

    refactor: Changing the default color of social to gray Adding hover effect Apply a slightly negative margin to align with the title

--- src/components/social/social.tsx
+++ src/components/social/social.tsx
@@ -7,14 +7,14 @@ import Link from "next/link";
 
 export default function Social() {
   return (
-    <div className="mt-4 flex gap-2">
-      <Link className="p-4" href="https://github.com/infinite-nil">
+    <div className="my-4 flex gap-2 text-gray-500 -ml-4">
+      <Link className="p-4 hover:text-blue-950" href="https://github.com/infinite-nil">
         <GitHubLogoIcon className="w-6 h-6" />
       </Link>
-      <Link className="p-4" href="https://www.linkedin.com/in/joao-amo/">
+      <Link className="p-4 hover:text-blue-950" href="https://www.linkedin.com/in/joao-amo/">
         <LinkedInLogoIcon className="w-6 h-6" />
       </Link>
-      <Link className="p-4" href="https://twitter.com/infinite_nil">
+      <Link className="p-4 hover:text-blue-950" href="https://twitter.com/infinite_nil">
         <TwitterLogoIcon className="w-6 h-6" />
       </Link>
     </div>
