9d2d384d59f6b4589dd50b1980ee065af83dd7e0
João Alberto <hey@joaoalberto.dev>
2024-01-05 18:20:29 -0300

    feat: adding projects components

---
+++ src/components/projects/project.tsx
@@ -0,0 +1,31 @@
+import { Repository } from "@/services/api/types";
+import { ArrowTopRightIcon } from "@radix-ui/react-icons";
+
+type Props = {
+  repository: Repository;
+};
+
+export default function Project({ repository }: Props) {
+  return (
+    <li
+      className="border-t-2 last:border-b-2 md:border md:flex-1 md:min-w-[calc(33%-4px)] md:max-w-[calc(33%-4px)] md:last:border md:h-[170px] hover:border-blue-950 group"
+      key={repository.html_url}
+    >
+      <a
+        target="_blank"
+        className="border border-transparent p-8 flex justify-stretch md:p-2 md:flex-col md:items-end md:justify-between md:gap-2 h-full group-hover:border-blue-950"
+        href={repository.html_url}
+      >
+        <span className="content flex-1 pr-4">
+          <span className="block text-lg md:mb-2 font-semibold">
+            {repository.name}
+          </span>
+          <span className="block text-sm text-gray-500">
+            {repository.description}
+          </span>
+        </span>
+        <ArrowTopRightIcon className="m-2" />
+      </a>
+    </li>
+  );
+}
---
+++ src/components/projects/projects.tsx
@@ -0,0 +1,20 @@
+import { Repository } from "@/services/api/types";
+import Subtitle from "../subtitle/subtitle";
+import Project from "./project";
+
+type Props = {
+  repositories: Repository[];
+};
+
+export default function Projects({ repositories }: Props) {
+  return (
+    <div className="mt-12 w-full">
+      <Subtitle>Projects</Subtitle>
+      <ul className="w-screen mt-4 -ml-8 flex-wrap md:w-full md:ml-0 md:flex md:gap-2">
+        {repositories.map((repository) => (
+          <Project key={repository.html_url} repository={repository} />
+        ))}
+      </ul>
+    </div>
+  );
+}
