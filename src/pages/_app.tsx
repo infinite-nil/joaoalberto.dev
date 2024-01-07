ce751351ae57402996069226c6d5eaaaec6b1464
João Alberto <hey@joaoalberto.dev>
2024-01-07 15:45:51 -0300

    feat: adding axe tool

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,16 @@ import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import { Inter, Syne } from "next/font/google";
+import React from 'react';
 
 const syne = Syne({ subsets: ["latin"], variable: "--font-syne" });
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
+if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
+  const ReactDOM = require('react-dom');
+  const axe = require('@axe-core/react');
+  axe(React, ReactDOM, 1000);
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
