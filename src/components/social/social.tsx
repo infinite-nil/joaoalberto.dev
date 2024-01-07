e04577189df7d750a0217c4d3fd98cb604f4d511
João Alberto <hey@joaoalberto.dev>
2024-01-07 15:47:19 -0300

    fix: adding missing aria-label to social links

--- src/components/social/social.tsx
+++ src/components/social/social.tsx
@@ -8,13 +8,13 @@ import Link from "next/link";
 export default function Social() {
   return (
     <div className="my-4 flex gap-2 text-gray-500 -ml-4">
-      <Link className="p-4 hover:text-blue-950" href="https://github.com/infinite-nil">
+      <Link aria-label="Github" className="p-4 hover:text-blue-950" href="https://github.com/infinite-nil">
         <GitHubLogoIcon className="w-6 h-6" />
       </Link>
-      <Link className="p-4 hover:text-blue-950" href="https://www.linkedin.com/in/joao-amo/">
+      <Link aria-label="Linkedin" className="p-4 hover:text-blue-950" href="https://www.linkedin.com/in/joao-amo/">
         <LinkedInLogoIcon className="w-6 h-6" />
       </Link>
-      <Link className="p-4 hover:text-blue-950" href="https://twitter.com/infinite_nil">
+      <Link aria-label="X/Twitter" className="p-4 hover:text-blue-950" href="https://twitter.com/infinite_nil">
         <TwitterLogoIcon className="w-6 h-6" />
       </Link>
     </div>
