984a62f0c2e864f6792c3041d4c6bca7777a0c2f
João Alberto <hey@joaoalberto.dev>
2024-01-17 12:10:20 -0300

    fix: subtitle eslint errors

--- src/components/subtitle/subtitle.tsx
+++ src/components/subtitle/subtitle.tsx
@@ -2,6 +2,8 @@ type Props = {
   children: string;
 };
 
-export default function Subtitle({ children }: Props) {
+function Subtitle({ children }: Props) {
   return <h3 className="text-xl font-title font-bold">{children}</h3>;
 }
+
+export { Subtitle };
